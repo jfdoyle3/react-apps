defae6c7aa466c86c08aba9acfe6bd826d053c4f
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2018-11-24 04:39:01 -0500

    started a more complex app

---
+++ myapp/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+       <h1>Helllooo CareerDevs</h1>
+       <p>Welcome :)</p>
+      </div>
+    );
+  }
+}
+
+export default App;
