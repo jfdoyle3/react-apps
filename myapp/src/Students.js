c2acc2373bae95bc7774057531da6e3bfc6b1c45
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2018-11-25 09:39:37 -0500

    various ways to use conditional outputs

--- myapp/src/Students.js
+++ myapp/src/Students.js
@@ -1,25 +1,65 @@
-import React, {Component} from 'react'
+import React from 'react';
 
-class Students extends Component{
-    render(){
-       // console.log(this.props)
-       const {students}=this.props
-       const studentList=students.map(student =>{
-           return ( 
-            <div className="student" key={student.id}>
-                <div>Name: {student.name}</div>
-                <div>Group: {student.group}</div>
-                <div>Time: {student.time}</div>
-            </div>
-            )
-       })
-        return(
-            <div className="student-list">
-                {studentList}        
-            </div>
-
-        )
+const Students = ({students}) => {
+    // This renders out the template directly. the other 2 below output the data through a varible.
+    // using a varible allows the the logic and template to be cleaner code.
+       return(
+           <div className="student-list">
+               {
+                   students.map(student=>{
+                       return student.group==='advanced' ? (
+                        <div className='student' key={student.id}>
+                           <div>Name: {student.name}</div>
+                           <div>Group: {student.group}</div>
+                           <div>Time: {student.time}</div>
+                        </div>
+                       ): null
+                   })
+               }
+           </div>
+       )
     }
-}
+//       using the IF statement 
+//       ------------------------------------       
+//       const studentList=students.map(student =>{
+//           if(student.group==='advanced'){
+//           return ( 
+//            <div className="student" key={student.id}>
+//                <div>Name: {student.name}</div>
+//                <div>Group: {student.group}</div>
+//                <div>Time: {student.time}</div>
+//            </div>
+//            )
+//           } else {
+//               return null
+//           }
+//       })
+//        return(
+//            <div className="student-list">
+//                {studentList}        
+//            </div>
+//        )
+//    }
+
+
+//  using a ternary operator
+//  -------------------------
+//const studentList=students.map(student =>{
+//   return student.group === 'advanced' ? (
+//                <div className="student" key={student.id}>
+//                   <div>Name: {student.name}</div>
+//                   <div>Group: {student.group}</div>
+//                   <div>Time: {student.time}</div>
+//               </div>
+//               ):null
+//})
+//        return(
+//            <div className="student-list">
+//                {studentList}        
+//            </div>
+//
+//        )
+//    }
+
 
 export default Students
