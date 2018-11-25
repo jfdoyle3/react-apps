89f170f119afcce36a94e7e2f1fe951e0b02fc55
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2018-11-25 07:41:14 -0500

    changed hard code objects to dynamic

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -2,12 +2,19 @@ import React, { Component } from 'react';
 import Classmates from './classmates';
 
 class App extends Component {
+  state={
+    classmates: [
+      {name:'jim', group: 'intermediate', time:'evening', id: 1},
+      {name:'juan', group: 'advanced', time:'evening', id: 2},
+      {name:'Leah', group: 'advanced', time:'evening', id: 3},
+    ]
+  }
   render() {
     return (
       <div className="App">
        <h1>Helllooo CareerDevs</h1>
        <p>Welcome :)</p>
-       <Classmates name="Jim" group="Intermediate" class="Evening" />
+       <Classmates classmates={this.classmates} />
       </div>
     );
   }
--- myapp/src/classmates.js
+++ myapp/src/classmates.js
@@ -2,12 +2,19 @@ import React, {Component} from 'react'
 
 class Classmates extends Component{
     render(){
-        return(
+       // console.log(this.props)
+       const {classmates}=this.props
+       const classmateList=classmastes.map(classmate =>{
+           return ( 
             <div className="student">
-                <div>Name: Jim</div>
-                <div>Group: Intermediate</div>
-                <div>Class: Evening</div>
+                <div>Name: {name}</div>
+                <div>Group: {group}</div>
+                <div>Time: {time}</div>
             </div>
+            )
+       })
+        return(
+
         )
     }
 }
