daf01be58b3c31c60c291cc10a42c90f259d1710
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2018-11-30 08:27:47 -0500

    stsrted a todo list

---
+++ todolist/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  state={
+    todos:[
+      {id: 1, content: 'buy beer'}
+      {id: 2, content: 'play game'}
+    ]
+  }
+  render() {
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  }
+}
+
+export default App;
