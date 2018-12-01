baf378c86a54d7d5cf31899b3ea1b5a1393d3bc7
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2018-12-01 07:58:38 -0500

    delete tasks function works

--- tasklist/src/App.js
+++ tasklist/src/App.js
@@ -9,11 +9,19 @@ class App extends Component {
       {id: 2, content: 'start coding'}
     ]
   }
+  deleteTask=(id)=>{
+    const tasks=this.state.tasks.filter(task=>{
+      return task.id !== id
+    })
+    this.setState({
+      tasks
+    })
+  }
   render() {
     return (
-      <div className="App">
+      <div className="task-app container">
         <h1 className="center blue-text">Tasks</h1>
-        <Tasks tasks={this.state.tasks} />
+        <Tasks tasks={this.state.tasks} deleteTask={this.deleteTask}/>
       </div>
     );
   }
--- tasklist/src/Tasks.js
+++ tasklist/src/Tasks.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
-const Tasks = ({tasks}) =>{
+const Tasks = ({tasks, deleteTask}) =>{
     
     const taskList= tasks.length ? (
         tasks.map(task =>{
             return (
              <div className="collection-item" key={task.id}>
-                 <span>{task.content}</span>
+                 <span onClick={()=> {deleteTask(task.id)}}>{task.content}</span>
              </div> 
             )
         })
