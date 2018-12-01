beae32cb0b457dab14b8a40ebbdefba8a62fa070
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2018-12-01 12:12:13 -0500

    Settng up Routers in react

---
+++ gadabout/components/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Contact=()=>{
+   return(
+    <div className="container">
+       <h4 className="center">Contact</h4>
+       <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis dolorum ducimus, neque earum quos deserunt esse sapiente atque culpa ipsam illum odio, dicta cumque qui numquam modi aspernatur quod ullam?</p>
+       
+    </div>
+   ) 
+}
+
+export default Contact
