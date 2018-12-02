c957d59e8b992e2b8fa92d325ac69066ec54ee5f
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2018-12-01 21:44:44 -0500

    lifecycle hook - connected to datasourcd

--- gadabout/src/components/Navbar.js
+++ gadabout/src/components/Navbar.js
@@ -2,8 +2,8 @@ import React from 'react'
 import {Link, NavLink, withRouter} from 'react-router-dom'
 
 const Navbar = (props) => {
-  setTimeout(()=>{
-    props.history.push('/about')}, 2000)
+ // setTimeout(()=>{
+ //   props.history.push('/about')}, 2000)
     return (
       <nav className="nav-wrapper blue darken-3">
         <div className="container">
