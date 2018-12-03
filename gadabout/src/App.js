30453e1e6d9ac0990abc4a2f4d3d7e4a1670ed3e
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2018-12-02 22:58:44 -0500

    added an image to the app

--- gadabout/src/App.js
+++ gadabout/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {BrowserRouter, Route} from 'react-router-dom'
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Home from './components/Home'
 import About from './components/About'
@@ -12,11 +12,13 @@ class App extends Component {
     return (
       <BrowserRouter>
       <div className="App">
-        <Navbar />
-      <Route exact path="/" component={Home} />
-      <Route path="/about" component={About} />
-      <Route path="/contact" component={Contact} />
-      <Route path="/:post_id" component={Post} />
+      <Navbar />
+      <Switch>
+         <Route exact path="/" component={Home} />
+         <Route path="/about" component={About} />
+         <Route path="/contact" component={Contact} />
+         <Route path="/:post_id" component={Post} />
+      </Switch>
       </div>
       </BrowserRouter>
     );
